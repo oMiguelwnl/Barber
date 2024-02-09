c527addb698d78742edae446cb0dd56c50f6fe20
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-02-09 10:18:44 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Header from "../_components/header";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Header />
     </div>
   );
 }
---
+++ app/_components/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import { Card, CardContent } from "../../components/ui/card";
+import { Button } from "../../components/ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Image src="/logo.png" alt="Logo image" height={18} width={120} />
+
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={16} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
