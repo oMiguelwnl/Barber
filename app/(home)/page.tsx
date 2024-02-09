0f12cd5001eaeb6ba83ffd68e1beb11539c1e116
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-02-09 10:59:42 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="font-['Nunito'] text-xl font-bold ">Olá, Miguel!</h2>
+        <p className="capitalize text-sm  font-['Nunito']">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
