5f652cfe6987e927404f3de1356d1fc154f304b7
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-02-12 12:13:38 -0300

    feat: add initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,14 +3,19 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  // GET Barbershops
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
       <div className="px-5 pt-5">
-        <h2 className="font-['Nunito'] text-xl font-bold ">Olá, Miguel!</h2>
-        <p className="capitalize text-sm  font-['Nunito']">
+        <h2 className=" text-xl font-bold ">Olá, Miguel!</h2>
+        <p className="capitalize text-sm  ">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
@@ -20,10 +25,21 @@ export default function Home() {
         <Search />
       </div>
       <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+        <h2 className="text-xs uppercase text-slate-500 font-bold mb-3 ">
           Agendamentos
         </h2>
         <BookingItem />
+      </div>
+      <div className="mt-6">
+        <h2 className="text-xs px-5 uppercase font-bold mb-3 text-slate-500 ">
+          Recomendados
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -7,22 +7,23 @@ const BookingItem = () => {
     <Card>
       <CardContent className="p-5 flex justify-between py-0">
         <div className="flex flex-col gap-2 py-5">
-          <Badge className="bg-[#1f1647] text-primary hover:bg-[#221C3D] w-fit">
+          <Badge className="bg-[#1f1647] text-primary hover:bg-[#221C3D] w-fit ">
             Confirmado
           </Badge>
-          <h2 className="font-bold">Corte de Cabelo</h2>
+          <h2 className="font-bold ">Corte de Cabelo</h2>
 
           <div className="flex items-center gap-2">
             <Avatar className="h-6 w-6">
               <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+
               <AvatarFallback>A</AvatarFallback>
             </Avatar>
 
-            <h3 className="text-sm">Vintage Barber</h3>
+            <h3 className="text-sm ">Vintage Barber</h3>
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center border-l border-solid border-secondary px-3">
+        <div className="flex flex-col items-center justify-center border-l border-solid border-secondary px-3 ">
           <p className="text-sm">Fevereiro</p>
           <p className="text-2xl">06</p>
           <p className="text-sm">09:45</p>
