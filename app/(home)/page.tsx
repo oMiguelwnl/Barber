00fb365f86409f38f817d52d514ef54caa3cadae
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-02-19 15:22:44 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,10 +5,29 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  // GET Barbershops
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? await db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              lt: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div>
@@ -26,12 +45,16 @@ export default async function Home() {
         <Search />
       </div>
 
-      {/* <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-slate-500 font-bold mb-3 ">
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs uppercase text-slate-500 font-bold mb-3 ">
           Agendamentos
         </h2>
-        <BookingItem />
-      </div> */}
+        <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+      </div>
 
       <div className="mt-6">
         <h2 className="text-xs px-5 uppercase font-bold mb-3 text-slate-500 ">
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -13,19 +13,32 @@ const BookingsPage = async () => {
     return redirect("/");
   }
 
-  const bookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-    },
-    include: {
-      service: true,
-      barbershop: true,
-    },
-  });
-
-  const confirmedBookings = bookings.filter((booking) => isFuture(booking.date));
-  const finishedBookings = bookings.filter((booking) => isPast(booking.date));
-
+  const [confirmedBookings, finishedBookings] = await Promise.all([
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          lt: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+  ]);
   return (
     <>
       <Header />
@@ -33,7 +46,9 @@ const BookingsPage = async () => {
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold">Agendamentos</h1>
 
-        <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Confirmados</h2>
+        <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
+          Confirmados
+        </h2>
 
         <div className="flex flex-col gap-3">
           {confirmedBookings.map((booking) => (
@@ -41,7 +56,9 @@ const BookingsPage = async () => {
           ))}
         </div>
 
-        <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
+        <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
+          Finalizados
+        </h2>
 
         <div className="flex flex-col gap-3">
           {finishedBookings.map((booking) => (
